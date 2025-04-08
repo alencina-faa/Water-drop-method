878e52aa4283faabca9e91d41877a7793a60a955
Alberto Lencina <alencina@azul.faa.unicen.edu.ar>
2025-04-08 17:49:41 -0300

    Camera class created
    A class called Camera is created to handle camera from app.py

---
+++ src/Water_drop_method/Camera.py
@@ -0,0 +1,72 @@
+import cv2
+
+class Camera:
+
+    global fourcc
+    fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
+
+    global out
+    out = False
+
+    def __init__(self, fps=None, width=None, height=None):#, path_name_save_video=None):
+        self.fps = fps if fps is not None else 1
+        self.width = width if width is not None else 640
+        self.height = height if height is not None else 480
+        self.path_name_save_video = "captured_video.avi"#path_name_save_video if path_name_save_video is not None else "captured_video.avi"
+
+        #global out
+        #out = cv2.VideoWriter(self.path_name_save_video, fourcc, self.fps, (self.width, self.height))    
+    
+
+    def set_path_name_save_video(self):#, path_name_save_video=None):
+        #self.path_name_save_video = path_name_save_video if path_name_save_video is not None else path_name_save_video
+        
+        global out
+        out = cv2.VideoWriter(self.path_name_save_video, fourcc, self.fps, (self.width, self.height))    
+               
+
+    def start(self, device=None):
+        self.device = device if device is not None else 0
+
+        global cap 
+        cap = cv2.VideoCapture(self.device)
+        if not cap.isOpened():
+            print("Cannot open camera")    
+            exit()
+    
+
+    def preview_camera(self, winname=None):
+        self.winname = winname if winname is not None else "Preview"
+        while True:
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+
+            # if frame is read correctly ret is True
+            if not ret:
+                print("Can't receive frame (stream end?). Exiting ...")
+                break
+    
+            # Display the resulting frame
+            cv2.imshow(self.winname, frame)
+            if cv2.waitKey(1) == ord('q'):
+                break
+    
+
+    def take_write_snapshot(self):
+        if not out:
+            self.set_path_name_save_video()
+        ret, frame = cap.read()
+        out.write(frame)
+
+    
+    def close_window():
+        cv2.destroyAllWindows()
+
+
+    def stop():
+        cap.release()
+        out.release()
+
+
+        
+    
--- src/Water_drop_method/app.py
+++ src/Water_drop_method/app.py
@@ -5,7 +5,7 @@ A colection of scripts to implement the Water drop method to determine the strut
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
-
+from Camera import Camera
 
 class WaterDropMethod(toga.App):
     def startup(self):
@@ -13,8 +13,7 @@ class WaterDropMethod(toga.App):
 
         Usually, you would add your application to a main content box.
         We then create a main window (with a name matching the app), and
-        show the main window.
-        """
+        show the main window."""
         
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.show()
@@ -43,3 +42,22 @@ def main():
 
 if __name__ == "__main__":
     main().main_loop()
+
+"""
+x = Camera(fps=1, width=640, height=480)
+
+x.start(device=0)
+print(x.path_name_save_video)
+#x.take_write_snapshot()
+x.path_name_save_video = "video.avi"
+print(x.path_name_save_video)
+x.preview_camera()
+
+for i in range(10):
+    x.take_write_snapshot()
+    print(i)
+
+#x.take_write_snapshot()
+x.close_window
+x.stop
+"""
